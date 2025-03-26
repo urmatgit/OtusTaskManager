7ec11272d41cc0b8551cf3b59136a82a9514a64c
urmatgit <urmat2g@gmail.com>
2025-03-26 16:58:21 +0300

    + postgresql

--- src/UserService.Api/Program.cs
+++ src/UserService.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using UserService.DataAccess;
+
 namespace UserService.Api
 {
     public class Program
@@ -13,6 +15,7 @@ namespace UserService.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddPersistance(builder.Configuration);
 
             var app = builder.Build();
 
--- src/UserService.Domain/DepencyInjections.cs
+++ src/UserService.Domain/DepencyInjections.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserService.DataAccess.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace UserService.DataAccess
 {
     public static class DepencyInjections
     {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddDbContext<TaskboardDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("TaskboardDb"));
+
+            });
+            return services;
+        }
     }
 }
