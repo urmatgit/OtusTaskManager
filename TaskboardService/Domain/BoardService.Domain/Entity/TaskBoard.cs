8bf96b7c02aa31d771c08800a8b9bef872fbe584
Никитин Павел Николаевич <tankist_84@bk.ru>
2025-05-22 15:09:35 +0600

    rename taskboard and hangfire services folders

---
+++ TaskboardService/Domain/BoardService.Domain/Entity/TaskBoard.cs
@@ -0,0 +1,41 @@
+﻿using BoardService.Domain.Abstraction;
+using BoardService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardService.Domain.Entity
+{
+    /// <summary>
+    /// Доска задач
+    /// </summary>
+    public class TaskBoard : IEntity<Guid>
+    {
+        /// <summary>
+        /// Идентифифкатор доски задач
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Название доски
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public required DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public BoartStatus Status { get; set; }
+
+        /// <summary>
+        /// Список колонок доски
+        /// </summary>
+        public IEnumerable<BoardColumn>? Columns { get; set; }
+    }
+}
---
+++ TaskboardService/Repository/Repository.Implementation/TaskBoardRepository.cs
@@ -0,0 +1,13 @@
+﻿using BoardService.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Repository.Abstractions;
+
+namespace Repository.Implementation
+{
+    class TaskBoardRepository : Repository<TaskBoard, Guid>, ITaskBoardRepository
+    {
+        public TaskBoardRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
