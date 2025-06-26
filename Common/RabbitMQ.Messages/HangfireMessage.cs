04594974ebc87048cc8fc61285810f479a579bb7
Никитин Павел Николаевич <tankist_84@bk.ru>
2025-06-26 15:21:51 +0600

    Добавлена библиотека с классами сообщений для брокера сообщений

---
+++ Common/RabbitMQ.Messages/HangfireMessage.cs
@@ -0,0 +1,24 @@
+﻿
+using Newtonsoft.Json;
+
+namespace RabbitMQ.Messages
+{
+    public class HangfireMessage
+    {
+        /// <summary>
+        /// Идентификатор пользователя, для которого надо запланировать задачу.
+        /// </summary>
+        [JsonProperty("userId")]
+        public Guid UserId { get; set; }
+        /// <summary>
+        /// Дата выполнения задачи.
+        /// </summary>
+        [JsonProperty("executionDate")]
+        public DateTime ExecutionDate { get; set; }
+        /// <summary>
+        /// Тип сервиса обрабатывающего задачу.        
+        /// </summary>
+        [JsonProperty("serviceType")]
+        public string ServiceType { get; set; } = string.Empty;
+    }
+}
---
+++ Common/RabbitMQ.Messages/NotifyMessage.cs
@@ -0,0 +1,25 @@
+﻿
+using Newtonsoft.Json;
+
+namespace RabbitMQ.Messages
+{
+    public enum MessageType : int 
+    {
+        UserRegistered,
+        UserProjectAdded,
+        UserProjectRemoved,
+        TaskAdded,
+        TaskCompleted,
+        TaskExpired
+    }
+
+    public class NotifyMessage
+    {
+        [JsonProperty("messageType")]
+        public MessageType MessageType { get; set; }
+        [JsonProperty("mailTo")]
+        public string MailTo { get; set; } = string.Empty;
+        [JsonProperty("messageData")]
+        public Dictionary<string, string> MessageData { get; set; } = [];
+    }
+}
