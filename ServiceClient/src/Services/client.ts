652dd3dc2f3d2c2d170bbf59e313c4616c8d2361
urmatgit <urmat2g@gmail.com>
2025-08-12 14:04:15 +0300

    changed ports

--- ServiceClient/src/Services/client.ts
+++ ServiceClient/src/Services/client.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5191/api',
+  baseURL: 'http://localhost:7080/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
